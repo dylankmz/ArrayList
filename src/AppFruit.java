0b380af7538b3b559ec41a3802cee8942ee5e6a7
dylankmz <dylankarapekmez@icloud.com>
2019-12-17 15:30:09 +0100

    Collections

--- src/AppFruit.java
+++ src/AppFruit.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class AppFruit {
 
@@ -6,7 +8,6 @@ public class AppFruit {
 
         ArrayList<Fruit> fruitmandje = new ArrayList<>();
 
-        fruitmandje.add(new Fruit());
         Fruit f1 = new Fruit();
         f1.setNaam("Kiwi");
         f1.setEetbaar(true);
@@ -17,10 +18,25 @@ public class AppFruit {
         f2.setEetbaar(false);
         fruitmandje.add(f2);
 
+        Fruit f3 = new Fruit();
+        f3.setNaam("Banaan");
+        f3.setEetbaar(true);
+        fruitmandje.add(f3);
+
         System.out.println(fruitmandje.toString());
 
         fruitmandje.get(0).isEetbaar();
-        fruitmandje.get(15243).isEetbaar();
 
+        for (Fruit f : fruitmandje) { //Over de fruitmandje loopen
+            System.out.println(f.getNaam());
+        }
+
+        Collections.sort(fruitmandje, new Comparator<Fruit>() {
+            @Override
+            public int compare(Fruit o1, Fruit o2) {
+                return o1.getNaam().compareTo(o2.getNaam());
+            }
+        });
     }
 }
+
